94973cc88b5d1489f3078105935f2c008a237ca9
PaulMares <fpmaresquiersanchez@dons.usfca.edu>
2024-10-15 15:41:13 -0700

    Changed to use new addWords method

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
-		String[] words = new String[]{"Hello", "helios", "world", "Wallaby", "worse", "worsen", "gaunt", "n00b"};
+		String[] words = new String[]{"Hello", "helios", "world", "Wallaby", "worse", "worsen", "gaunt", "n00b", "order"};
 
 		PrefixDictionary dict = new PrefixDictionary();
-		for (String word : words) {
-			dict.addWord(word);
-		}
+		dict.addWords(words);
 
 		System.out.println(dict);
 		System.out.println(dict.checkWord("helio"));
+
+		System.out.println(Similarity.lcs(words[2], words[8]));
 	}
 }
