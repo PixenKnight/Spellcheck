d13edf85abfeb5fea7339652fdebaa2e0421e1dc
PaulMares <fpmaresquiersanchez@dons.usfca.edu>
2024-10-16 09:45:29 -0700

    Began implementing argument parsing

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import java.io.IOException;
 import java.nio.file.Path;
 
+import com.paul.ArgumentParser;
+
 public class Main {
 	public static void main(String[] args) {
-		String[] words = new String[]{"Hello", "helios", "world", "Wallaby", "worse", "worsen", "gaunt", "n00b", "order", "hell", "zebra"};
+		ArgumentParser parser = new ArgumentParser(args);
+
+		if (parser.hasFlag("-input")) {
+
+		} else {
+			throw new RuntimeException("No input filepath specified");
+		}
 
 		PrefixDictionary dict = new PrefixDictionary();
-		dict.addWords(words);
 
 		System.out.println(dict);
 		System.out.println(dict.checkWord("helios"));
