b658ed8d2a6c2a3b29dbe2d91b871864c8b68f52
PaulMares <fpmaresquiersanchez@dons.usfca.edu>
2024-10-15 21:53:33 -0700

    Updated to have file writing

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class Main {
 	public static void main(String[] args) {
-		String[] words = new String[]{"Hello", "helios", "world", "Wallaby", "worse", "worsen", "gaunt", "n00b", "order"};
+		String[] words = new String[]{"Hello", "helios", "world", "Wallaby", "worse", "worsen", "gaunt", "n00b", "order", "hell", "zebra"};
 
 		PrefixDictionary dict = new PrefixDictionary();
 		dict.addWords(words);
 
 		System.out.println(dict);
-		System.out.println(dict.checkWord("helio"));
+		System.out.println(dict.checkWord("helios"));
+		try {
+			dict.toFile(Path.of("dict.txt"));
+		} catch (IOException ignored) {}
 
-		System.out.println(Similarity.lcs(words[2], words[8]));
+		//System.out.println(Similarity.lcs(words[2], words[8]));
 	}
 }
